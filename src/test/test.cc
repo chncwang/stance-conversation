a20311deb6e33e6fad96b05a7f4e36c370424e12
Chauncey Wang <chncwang@gmail.com>
2020-09-16 20:37:14 +0800

    matrix concat tested

--- src/test/test.cc
+++ src/test/test.cc
@@ -1,53 +1,109 @@
 #include "N3LDG.h"
+#include <memory>
+
+constexpr int dim = 2000;
 
 struct ModelParams : public N3LDGSerializable, public TunableCombination<BaseParam>
 #if USE_GPU
 , public TransferableComponents
 #endif
 {
-    LookupTable<Param> lookup_table;
-    UniParams hidden_to_wordvector_params;
-    TransformerEncoderParams transformer_encoder_params;
-    LSTM1Params left_to_right_decoder_params;
-    AdditiveAttentionParams attention_params;
+    vector<UniParams*> params;
+    UniParams output_param;
 
-    ModelParams() : lookup_table("lookup_table"),
-    hidden_to_wordvector_params("hidden_to_wordvector_params"),
-    transformer_encoder_params("encoder"), left_to_right_decoder_params("decoder"),
-    attention_params("attention_params") {}
+    ModelParams() : output_param("output_param") {
+        for (int i = 0; i < 10; ++i) {
+            UniParams* param = new UniParams(to_string(i));
+            param->init(dim, dim);
+            params.push_back(param);
+        }
+        output_param.init(3, 10 * dim);
+    } 
 
     Json::Value toJson() const override {
         Json::Value json;
-        json["lookup_table"] = lookup_table.toJson();
-        json["hidden_to_wordvector_params"] = hidden_to_wordvector_params.toJson();
-        json["transformer_encoder_params"] = transformer_encoder_params.toJson();
-        json["left_to_right_decoder_params"] = left_to_right_decoder_params.toJson();
-        json["attention_params"] = attention_params.toJson();
         return json;
     }
 
     void fromJson(const Json::Value &json) override {
-        lookup_table.fromJson(json["lookup_table"]);
-        hidden_to_wordvector_params.fromJson(json["hidden_to_wordvector_params"]);
-        transformer_encoder_params.fromJson(json["left_to_right_encoder_params"]);
-        left_to_right_decoder_params.fromJson(json["left_to_right_decoder_params"]);
-        attention_params.fromJson(json["attention_params"]);
     }
 
 #if USE_GPU
-    std::vector<n3ldg_cuda::Transferable *> transferablePtrs() override {
-        return {&lookup_table, &hidden_to_wordvector_params, &transformer_encoder_params,
-            &left_to_right_decoder_params, &attention_params};
+    vector<n3ldg_cuda::Transferable *> transferablePtrs() override {
+        vector<n3ldg_cuda::Transferable *> ptrs = {&output_param};
+        for (auto x : params) {
+            ptrs.push_back(x);
+        }
+        return ptrs;
     }
 #endif
 
 protected:
-    virtual std::vector<Tunable<BaseParam>*> tunableComponents() override {
-        return {&lookup_table, &hidden_to_wordvector_params, &transformer_encoder_params,
-            &left_to_right_decoder_params, &attention_params};
+    virtual vector<Tunable<BaseParam>*> tunableComponents() override {
+        vector<Tunable<BaseParam> *> ptrs = {&output_param};
+        for (auto x : params) {
+            ptrs.push_back(x);
+        }
+        return ptrs;
     }
 };
 
 int main() {
+#if USE_GPU
+    n3ldg_cuda::InitCuda(0, 0);
+#endif
+    ModelParams params;
+#if !USE_GPU
+    CheckGrad grad_checker;
+    grad_checker.init(params.tunableParams());
+#endif
+    ModelUpdate optimizer;
+    optimizer.setParams(params.tunableParams());
+    optimizer._alpha = 1e-5;
+
+    auto f = [&](int answer) {
+        shared_ptr<Graph> graph(new Graph);
+        Node *bucket = n3ldg_plus::bucket(*graph, dim, 1);
+        vector<Node *> ys;
+        for (int i = 0; i < 10; ++i) {
+            vector<Node *> nodes;
+            for (int j = 0; j < 10 - i; ++j) {
+                Node *x = n3ldg_plus::linear(*graph, *params.params.at(j), *bucket);
+                nodes.push_back(x);
+            }
+            Node * y = n3ldg_plus::concatToMatrix(*graph, nodes);
+            if (i > 0) {
+                y = n3ldg_plus::concat(*graph, {y, n3ldg_plus::bucket(*graph, i * dim, 0)});
+            }
+            ys.push_back(y);
+        }
+        Node *output = n3ldg_plus::averagePool(*graph, ys);
+        output = n3ldg_plus::linear(*graph, params.output_param, *output);
+        output = n3ldg_plus::softmax(*graph, *output);
+        vector<Node *> v = {output};
+        graph->compute();
+        output->getVal().print();
+        cout << "answer:" << answer << endl;
+        dtype loss = crossEntropyLoss(v, {answer}, 1);
+        cout << "loss:" << loss << endl;
+        return make_pair(graph, loss);
+    };
+
+    for (int i = 0; i < 1000; ++i) {
+        int answer = i % 3;
+        auto p = f(answer);
+        p.first->backward();
+
+#if !USE_GPU
+        auto loss_function = [&](const int &p) -> dtype {
+            return f(answer).second;
+        };
+        vector<int> nulls = {1};
+        grad_checker.check<int>(loss_function, nulls, "");
+#endif
+
+        optimizer.updateAdam();
+    }
+
     return 0;
 }
